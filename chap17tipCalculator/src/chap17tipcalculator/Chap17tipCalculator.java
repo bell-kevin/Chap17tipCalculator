2492b14722879f788b8f4c93f36b9c78b8a8846b
Kevin Bell <bellKevin@pm.me>
2022-09-27 08:57:44 -0600

    Update Chap17tipCalculator.java

--- chap17tipCalculator/src/chap17tipcalculator/Chap17tipCalculator.java
+++ chap17tipCalculator/src/chap17tipcalculator/Chap17tipCalculator.java
@@ -6,8 +6,41 @@
  **************************************************************************** */
 package chap17tipcalculator;
 
-public class Chap17tipCalculator {
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class Chap17tipCalculator extends JFrame {
+    
+    private static final int WIDTH = 325;
+    private static final int HEIGHT = 100;
+    private JTextField mealCostBox; // holds user's meal cost
+    private JTextField output; // displays tip amount calculation
+//******************************************************************************
+
+    public Chap17tipCalculator() {
+        setTitle("Tip Calculator");
+        setSize(WIDTH, HEIGHT);
+        setLayout(new FlowLayout());
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        createContents();
+        setVisible(true);
+    } // end constructor method
+//******************************************************************************
+    // Create components and add them to window
+
+    private void createContents() {
+        JLabel mealCostPrompt = new JLabel("How much did the meal cost?");
+        mealCostBox = new JTextField(15);
+        output = new JTextField(10);
+        add(mealCostPrompt);
+        add(mealCostBox);
+        add(output);
+        mealCostBox.addActionListener(new Listener());
+    } // end createContents method
+//******************************************************************************
 
     public static void main(String[] args) {
+        new tipCalculator();
     } // end main method
 } // end Chap17tipCalculator class
