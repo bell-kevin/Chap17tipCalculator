cf5e8c5b3a37a905c75722a304865e57ea636f01
Kevin Bell <bellKevin@pm.me>
2022-09-27 09:23:39 -0600

    Update Chap17tipCalculator.java

--- chap17tipCalculator/src/chap17tipcalculator/Chap17tipCalculator.java
+++ chap17tipCalculator/src/chap17tipcalculator/Chap17tipCalculator.java
@@ -11,11 +11,11 @@ import java.awt.*;
 import java.awt.event.*;
 
 public class Chap17tipCalculator extends JFrame {
-    
+
     private static final int WIDTH = 325;
     private static final int HEIGHT = 100;
     private JTextField mealCostBox; // holds user's meal cost
-    private JTextField output; // displays tip amount calculation
+    private JTextField outputBox; // displays tip amount calculation
 //******************************************************************************
 
     public Chap17tipCalculator() {
@@ -27,25 +27,33 @@ public class Chap17tipCalculator extends JFrame {
         setVisible(true);
     } // end constructor method
 //******************************************************************************
-    // Create components and add them to window
 
-    private void createContents() {
+    private void createContents() {  // Create components and add them to window
         JLabel mealCostPrompt = new JLabel("How much did the meal cost?");
+        JButton button1 = new JButton("10% tip");
+        JButton button2 = new JButton("15% tip");
+        JButton button3 = new JButton("20% tip");
         mealCostBox = new JTextField(15);
-        output = new JTextField(10);
+        outputBox = new JTextField(10);
+        outputBox.setEditable(false);
         add(mealCostPrompt);
         add(mealCostBox);
-        add(output);
+        add(outputBox);
+        add(button1);
+        add(button2);
+        add(button3);
         mealCostBox.addActionListener(new Listener());
     } // end createContents method
 //******************************************************************************
-    // Inner class for event handling
-    private class Listener implements ActionListener{
-        public void actionPerformed(ActionEvent e){
-            
+
+    private class Listener implements ActionListener {//InnerClass eventHandling
+
+        public void actionPerformed(ActionEvent e) {
+
         } // end actionPerformed
     } // end Listener class
 //******************************************************************************
+
     public static void main(String[] args) {
         new tipCalculator();
     } // end main method
